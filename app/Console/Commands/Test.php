edfe196c8a0615aeaf5ac76d42e4c9747ce25541
RizwanQutubuddin <rizwan.qutubuddin@gmail.com>
2023-03-01 16:24:55 +0530

    cron job

---
+++ app/Console/Commands/Test.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Test extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:add';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'User added';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $data["name"]="Dharma Chauhan";
+        $data["email"]="Dharmachauhan@gmail.com";
+        \DB::table('users')->insert($data);
+
+        $this->info('success');
+    }
+}
